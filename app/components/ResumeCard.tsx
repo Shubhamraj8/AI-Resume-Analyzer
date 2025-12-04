ab51920ce706f1ff8db9f2f210b03b7fdf2996d5
Shubham Raj Rathaur <94780982+Shubhamraj8@users.noreply.github.com>
2025-12-04 10:33:45 +0000

    in progress

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const ResumeCard = () => {
+  return (
+    <Link>
+      ResumeCard
+    </Link>
+  )
+}
+
+export default ResumeCard
