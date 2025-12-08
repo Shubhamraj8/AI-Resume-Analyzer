463f0f9e52c8b67d6153856c107dd9657fc95b8c
Shubham Raj Rathaur <94780982+Shubhamraj8@users.noreply.github.com>
2025-12-08 10:38:20 +0000

    built resume cards

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
 import { Link } from 'react-router'
 
-const ResumeCard = () => {
+const ResumeCard = ({resume}:{resume:Resume}) => {
   return (
-    <Link>
-      ResumeCard
+    <Link to={`/resume/${resume.id}`} className="resume-card animate-in fade-in duration-1000">
+      <div className="flex flex-col gap-2">
+        <h2 className="!text-black font-bold break-words">
+          {resume.companyName}
+        </h2>
+        <h3 className="textx-lg break-words text-gray-500">
+          {resume.jobTitle}
+        </h3>
+      </div>
     </Link>
   )
 }
